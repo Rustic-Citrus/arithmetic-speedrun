5916da26b0e84d7b67b83983f2acaae49bd75a79
Rustic-Citrus <mrcurtis.english@gmail.com>
2023-07-06 21:46:47 -0300

    Changed difficulty settings
    Changed formatting of graph

--- problems.py
+++ problems.py
@@ -15,19 +15,19 @@ low_score_sound = pyglet.media.load("media/low_score_sound.wav",
 
 def get_difficulty() -> int:
     """Reads the difficulty settings to the user and requests an integer 
-    choice for difficulty, ranging from 1 to 4. If the user enters a valid 
+    choice for difficulty, ranging from 1 to 6. If the user enters a valid 
     integer, it is returned with this function."""
     with open("difficulty.txt", "r") as f:
         difficulty_settings = f.readlines()
     print("".join(difficulty_settings))
     user_setting = ""
-    while (not isinstance(user_setting, int) or user_setting > 4 or 
+    while (not isinstance(user_setting, int) or user_setting > 6 or 
            user_setting < 1):
         user_setting = int(input("Choose a difficulty setting: \n>: "))
         if not isinstance(user_setting, int):
             print("TypeError: difficulty argument should be an integer.")
-        elif (user_setting > 4 or user_setting < 1):
-            print("ValueError: difficulty should be between 1 and 4.")
+        elif (user_setting > 6 or user_setting < 1):
+            print("ValueError: difficulty should be between 1 and 6.")
     return user_setting
 
 def get_problem(difficulty: int = 1) -> tuple:
@@ -37,13 +37,21 @@ def get_problem(difficulty: int = 1) -> tuple:
         x = y = 0
         match difficulty:
             case 1:
-                x, y = [random.randint(1, 10) for n in range(2)]
+                x, y = [random.randint(1, 9) for n in range(2)]
             case 2:
-                x, y = [random.randint(10, 100) for n in range(2)]
+                x = random.randint(10, 99)
+                y = random.randint(1, 9)
             case 3:
-                x, y = [random.randint(100, 1000) for n in range(2)]
+                x = random.randint(100, 999)
+                y = random.randint(1, 9)
             case 4:
-                x, y = [random.randint(1000, 10000) for n in range(2)]
+                x, y = [random.randint(10, 99) for n in range(2)]
+            case 5:
+                x = random.randint(10, 99)
+                y = random.randint(100, 999)
+            case 6:
+                x, y = [random.randint(100, 999) 
+                        for n in range(2)]
         solution = x * y
         equation = f"{x} * {y} = x"
         return (equation, solution)
@@ -97,12 +105,11 @@ def get_progress(username: str, difficulty: int) -> None:
     fig, axs = plt.subplots(2, 1)
     fig.suptitle(f"{username} - Scores")
     axs[0].plot(user_data["Timestamp"], user_data["Score"])
-    axs[0].set_xlabel("datetime")
-    axs[0].set_ylabel("score")
+    axs[0].set_ylabel("Score")
     axs[0].tick_params(axis='x', rotation=45)
     axs[1].plot(user_data["Timestamp"], user_data["Elapsed Time"])
-    axs[1].set_xlabel("datetime")
-    axs[1].set_ylabel("elapsed time")
+    axs[1].set_xlabel("Date & Time")
+    axs[1].set_ylabel("Time Taken")
     axs[1].tick_params(axis='x', rotation=45)
     fig.tight_layout()
     plt.show()
